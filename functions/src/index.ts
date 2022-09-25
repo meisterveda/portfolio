6a7574a32b3b15dbad0af657614dafad54ce9b31
Gustavo Cabezal <meisterveda@gmail.com>
2022-09-25 11:41:46 -0400

    functions updates

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,24 @@
-import * as functions from "firebase-functions";
+/* eslint-disable */
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import * as functions from 'firebase-functions'
+import axios from 'axios'
+import * as admin from 'firebase-admin'
+
+admin.initializeApp()
+
+const firestore = admin.firestore()
+
+exports.getDevtoUserArticles = functions.https.onRequest(async (req, res) => {
+    try {
+        await getUserArticles()
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+async function getUserArticles() {
+    const response = await axios.get(
+        'https://dev.to/api/articles?username=meisterveda&state=fresh'
+    )
+    console.log(JSON.stringify(response.data))
+}
